f79285864ef76225249f5e239440f65b94e0f07e
Julygh1 <Julymisc66@gmail.com>
2025-05-21 02:09:31 +0000

    draft

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ console.log (category);
 console.log (grp_size);
 console.log (paym_mode);
 
-function ReqSubmit(){
-    /* var checkboxes = document.getElementsByName("category");*/
-      var check_cat = document.getElementsByName("category");
-            var selectedValues = [];
-            
-            check_cat.forEach(checkbox => {
-                if (checkbox.checked) {
-                    selectedValues.push(checkbox.value);
-                }
-            });
+function getSelection() {
+/* cater for other category type */
+    document.getElementById("otherCatCheckbox").addEventListener("change", function() {
+            document.getElementById("otherCat").style.display = this.checked ? "inline-block" : "none";
+        });
 
-    var resultText = selectedValues.length 
-        ? "You selected: " + selectedValues.join(", ") 
-        : "Please select at least one exercise."; 
+    var checkcatboxes = document.getElementsByName("category");
+    var selectedcatValues = [];
 
-        document.getElementById("result").innerText = resultText; 
+    checkcatboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            selectedcatValues.push(checkbox.value);
+            }
+        });
 
+    var otherCatCheckbox = document.getElementById("otherCatCheckbox");
+    var otherCatInput = document.getElementById("otherCat");
+
+    if (otherCatCheckbox.checked && otherCatInput.value.trim() !== "") {
+        selectedcatValues.push(otherCatInput.value.trim());
+    }
+
+    var resultCatText = selectedcatValues.length 
+        ? "Your category selected: " + selectedcatValues.join(", ") 
+        : "Please select at least one exercise.";
+
+
+/* group size selection */
   var check_size = document.getElementsByName("groupsize");
             var selectedgroupsizeValues = [];
             
@@ -35,23 +46,36 @@ function ReqSubmit(){
         ? "You selected group size of: " + selectedgroupsizeValues.join(", ") 
         : "Please select at least one exercise."; 
 
-        document.getElementById("sizeresult").innerText = resultgroupsizeText; 
         
-    /* check on payment selection */    
-  var check_paym = document.getElementsByName("payment");
-            var selectedpaymentValues = [];
-            
-            check_paym.forEach(checkbox => {
-                if (checkbox.checked) {
-                    selectedpaymentValues.push(checkbox.value);
-                }
-            });
 
-    var resultpaymentText = selectedpaymentValues.length 
-        ? "You selected payment mode of: " + selectedpaymentValues.join(", ") 
-        : "Please select at least one exercise."; 
 
-        document.getElementById("paymresult").innerText = resultpaymentText; 
+/* cater for other payment mode */
+    document.getElementById("otherPaymCheckbox").addEventListener("change", function() {
+            document.getElementById("otherPaym").style.display = this.checked ? "inline-block" : "none";
+        });
 
+    var checkpaymboxes = document.getElementsByName("payment");
+    var selectedpaymValues = [];
+
+    checkpaymboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            selectedpaymValues.push(checkbox.value);
+            }
+        });
+
+    var otherPaymCheck = document.getElementById("otherPaymCheckbox");
+    var otherPaymInput = document.getElementById("otherPaym");
+
+    if (otherPaymCheck.checked && otherCatInput.value.trim() !== "") {
+        selectedpaymValues.push(otherPaymInput.value.trim());
+    }
+
+    var resultPaymText = selectedpaymValues.length 
+        ? "You selected: " + selectedpaymValues.join(", ") 
+        : "Please select at least one exercise.";
+
+/* show selection results */   
+    document.getElementById("result").innerText = resultCatText;     
+    document.getElementById("sizeresult").innerText = resultgroupsizeText; 
+    document.getElementById("paymresult").innerText = resultPaymText;
 }
-
